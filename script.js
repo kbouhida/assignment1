4c5eb49a2cfa181c991a43672de213fa79fa5e69
Katie <MT2C1@ac.sdmesa.net>
2017-02-15 19:59:08 -0800

    Assignment1
    Created HTML 5 and linked with script file

---
+++ script.js
@@ -0,0 +1,296 @@
+//TASK 1
+
+//var someMonth;
+//function(theMonth);               // function to return current month
+//const CURRENTMONTH;              // a constant
+//var summerMonth;                // an array of summer months
+//function myLibraryAfunction;   // a function 
+
+//**********************************************************************************************
+
+//TASK 2
+
+//12.2                          // NUMERIC LITERAL EXPRESSION
+//"Hello World!"                // STRING LITERAL EXPRESSION
+//true                          // BOOLEAN LITERAL EXPRESSION
+//null                          // NULL LITERAL EXPRESSION
+
+//***********************************************************************************************
+
+//TASK 3
+
+//var expressionOne = 2 + (6/2) + 1; 
+//var expressionTwo = Math.PI * 3;
+//var lastExpression = "(" + expressionOne + ") / (" + expressionTwo + ")";
+
+//***********************************************************************************************
+
+//TASK 4
+
+//var firstName, lastName, Address, City, State, zipCode, yourAge, referralSource, mayWeContactYou;
+
+//***********************************************************************************************
+    
+//TASK 5
+
+// METHOD 1
+    
+/* var firstName = "Katie", lastName = "Bouhida", Address = 1234 + " ABCD St", City = "San Diego", State = "CA", zipCode = 92109, yourAge = 21, referralSource = "Google", mayWeContactYou = "No";*/
+
+    
+// METHOD 2
+
+/*
+var firstName = "Katie";
+var lastName = "Bouhida"; 
+var Address = 1234 + " ABCD St"; 
+var City = "San Diego"; 
+var State = "CA"; 
+var zipCode = 92109; 
+var yourAge = 21; 
+var referralSource = "Google";
+var mayWeContactYou = "No";
+*/
+
+// METHOD 3
+
+/*
+var firstName;
+var lastName; 
+var Address; 
+var City; 
+var State; 
+var zipCode; 
+var yourAge; 
+var referralSource;
+var mayWeContactYou;
+
+firstName = "Katie";
+lastName = "Bouhida"; 
+Address = 1234 + " ABCD St"; 
+City = "San Diego"; 
+State = "CA"; 
+zipCode = 92109; 
+yourAge = 21; 
+referralSource = "Google";
+mayWeContactYou = "No";
+*/
+
+//**********************************************************************************************
+
+//TASK 6
+
+//var firstVariable;                             //STEP 1
+//var firstVariable = 123 + " Aero Dr";          //STEP 2
+//window.console.log(firstVariable);             //STEP 3
+
+//var secondVariable;                           //STEP 1
+//var secondVariable = "My name is " + 5 > 6;   //STEP 2
+//window.console.log(secondVariable);           //STEP 3 
+
+//var thirdVariable;                            //STEP 1
+//var thirdVariable = 5 + (5 > 6);              //STEP 2
+//window.console.log(thirdVariable);            //STEP 3
+
+//**********************************************************************************************
+
+//TASK 7
+
+//var color = "green";                                        //STEP 1
+//console.log("The car is a " + color + " " + model + ".");   //STEP 2
+//var model = "Honda";                                        //STEP 3
+
+//*********************************************************************************************
+
+// TASK 8
+
+/*var someString = "Who once said,\nOnly two things are infinite, the universe and human stupidity, and I'm not sure about the former.";
+window.console.log(someString);*/
+
+//**********************************************************************************************
+
+//TASK 9
+
+//NULL VALUE
+
+/* 
+var x = 100;
+x = null;
+window.console.log(x); 
+*/
+
+
+//UNDEFINED VALUE 
+/*
+var x;
+window.console.log(x);
+*/
+
+//*********************************************************************************************
+
+//TASK 10
+
+/*
+var a = "Katie";
+typeof a;
+window.console.log(typeof a);       //RETURNS A STRING
+*/
+
+/*
+var b = 21;
+typeof b;
+window.console.log(typeof b);       // RETURNS A NUMBER
+*/
+
+/*
+var c = 5 < 1;
+typeof c;
+window.console.log(typeof c);       // RETURNS A BOOLEAN
+*/
+
+
+//var d = [1, 2, 3, 4];
+//typeof d;
+//window.console.log(typeof d);      // RETURNS AN OBJECT
+
+
+/*
+var e 
+typeof e;
+window.console.log(typeof e);       // RETURNS UNDEFINED
+*/
+
+//********************************************************************************************
+
+//TASK 11
+
+//STEP 1 
+
+//alert("Hello " + "Zak Ruvalcaba, welcome to the JavaScript class!");
+
+//STEP 2
+
+/*alert("Hello " + "Katie Bouhida" + ", welcome to the JavaScript class!");*/
+
+
+//*********************************************************************************************
+
+//TASK 12
+
+//STEP 1 
+//var name = "Katie Bouhida";
+
+//STEP 2
+//alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+//*********************************************************************************************
+
+//TASK 13 
+
+//STEP 1
+//var course = "Javascript";
+//var name = "Katie Bouhida";
+//alert("Hello " + name + ", welcome to the" + course + " class!");
+
+
+//**********************************************************************************************
+
+//TASK 14
+
+//var course = "Javascript";
+//var name = "Katie Bouhida";
+//alert("Hello " + name + "." + "\n" + "Welcome to the " + course + " class!");
+
+//*********************************************************************************************
+
+//TASK 15
+
+//var name = prompt("What is your name?");
+//alert("Hello " + name);
+
+//*********************************************************************************************
+
+
+//TASK 16
+
+//var name = prompt("What is your name?");
+//var course = prompt("What class are you taking?");
+//alert("Hello " + name + "." + "\n" + "Welcome to the " + course + " class!");
+
+//*********************************************************************************************
+
+//TASK 17
+
+//var x = 10;                 //STEP 1
+//var y = 20;                 //STEP 2
+//console.log(x + y);         //DISPLAYS THE SUM OF X + Y
+
+
+//********************************************************************************************
+
+//TASK 18 
+
+//var x = 20;               //STEP 1
+//x += 20;                  //STEP 2
+//window.console.log(x);    //STEP 3
+
+//********************************************************************************************
+
+//TASK 19 
+
+//var x = 20;               //STEP 1
+//x *= 5;                   //STEP 2
+//window.console.log(x);    //STEP 3
+
+
+//*******************************************************************************************
+
+//TASK 20
+
+//var x = 20 % 3;           //STEP 1
+//x /= 1;                   //STEP 2
+//window.console.log(x);    //STEP 3
+
+
+//*******************************************************************************************
+
+//TASK 21
+
+//var x = 20;                           //STEP 1
+//var y = 10;                           //STEP 2
+//window.console.log(x > y !=false);   //STEP 3
+
+//*******************************************************************************************
+
+//TASK 22
+
+//var x = 20;                            //STEP 1
+//var y = 10;                           //STEP 2
+//window.console.log(x < y !=false);    //STEP 3
+
+//*******************************************************************************************
+
+//TASK 23
+
+//var x = new Object();
+//window.console.log(typeof x);
+
+//******************************************************************************************
+
+//TASK 24
+
+//function C() {}
+//function D() {}
+//var o = new C();
+//window.console.log(o instanceof C);
+
+//********************************************************************************************
+
+//TASK 25
+
+
+//function C() {}
+//function D() {}
+//var o = new C();
+//window.console.log(o instanceof D);
